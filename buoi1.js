dfbc7998c1296584b4139693f1695f577d909bf9
Trang Vu <anthaidistributor@gmail.com>
2020-05-22 19:17:28 +0700

    buoi 2

---
+++ buoi1.js
@@ -0,0 +1,25 @@
+//console.log("buoi 1")
+//var a = "Nguyen Van Teo"
+// 2 kieu du lieu
+// Primities type ( Kieu nguyen thuy) khong co su dung chuyen doi du lieu
+// Opject
+//console.log(a)
+//var nguyenVanA = {name : 'Nguyen Van A', age : 20}
+//console.log(nguyenVanA.name)
+//console.log(nguyenVanA['name'])
+// TH 2:  nếu truy vấn đến 1 thuộc tính không tồn tại Undefined
+//4. Array
+//var array.Names = ["Teo" ,"Ti","Tun"]
+//Toán tử : 
+//a+b=5
+//a,b toán hạng , +,-,*,/ ( chia lấy nguyên) % ( chia lấy dư) toán tử
+//var a = 10
+//var b = 5
+//console.log(a/b)
+//console.log(a%b)
+//var c = a++ - b++ + --b + b++
+//5. FUNTION :
+//function showName(){}
+// Toán tử so sánh :
+// If else
+// Co 6 gia tri tuong trung cho False : var a = false, 0, "", null, undefined, NaN ( not a number)
---
+++ buoi2.js
@@ -0,0 +1,10 @@
+//null
+//var(a)=null
+//console.log(a)
+//undefined
+//TH 1 : khai báo biến nhưng ko có giá trị
+//JSON
+var a 
+console.log(a)
+var teo = {name : 'nguyen van teo', age: 20}
+console.log(teo)
